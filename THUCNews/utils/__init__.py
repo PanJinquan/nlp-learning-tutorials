13fcb40772cd2d93f8f6f1800879e04fa3a3f51c
ubuntu <pan_jinquan@163.com>
2018-11-29 19:25:57 +0800

    ADD:

--- THUCNews/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*-coding: utf-8 -*-
-"""
-    @Project: nlp-learning-tutorials
-    @File   : __init__.py.py
-    @Author : panjq
-    @E-mail : pan_jinquan@163.com
-    @Date   : 2018-11-11 10:05:57
-"""
